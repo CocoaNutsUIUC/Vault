ab0e71052105e06157ae4a4324b56ec4f4d62cca
Justin Loew <jloloew@gmail.com>
2015-10-15 01:27:14 -0500

    add slides

---
+++ Vault/TouchIDViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TouchIDViewController.swift
+//  Vault
+//
+//  Created by Justin Loew on 10/6/15.
+//  Copyright © 2015 Justin Loew. All rights reserved.
+//
+
+import UIKit
+
+class TouchIDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
